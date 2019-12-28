ba39e32e3ae0b61b121b6c926551f229c414d2eb
Rickard Antonsson <rickard.antonsson@ikea.com>
2019-12-28 22:38:56 +0100

    Add home controller and views

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Error()
+		{
+			return View();
+		}
+	}
+}
